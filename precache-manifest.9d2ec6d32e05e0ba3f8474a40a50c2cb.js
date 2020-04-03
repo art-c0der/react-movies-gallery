e6b0b7455227d5cdac2d761f89d9f8314644ea24
Temu4 <artem.vorobjov@gmail.com>
2020-04-04 01:27:43 +0300

    Updates

---
+++ precache-manifest.9d2ec6d32e05e0ba3f8474a40a50c2cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2fbdf899ae6695a819988eed1a1c10d",
+    "url": "/react-movies-gallery/index.html"
+  },
+  {
+    "revision": "a5b2cd3fc2f32d807e2c",
+    "url": "/react-movies-gallery/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "cdc89b1a651640a32fef",
+    "url": "/react-movies-gallery/static/css/main.3db28624.chunk.css"
+  },
+  {
+    "revision": "a5b2cd3fc2f32d807e2c",
+    "url": "/react-movies-gallery/static/js/2.23d2e975.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-movies-gallery/static/js/2.23d2e975.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdc89b1a651640a32fef",
+    "url": "/react-movies-gallery/static/js/main.36405709.chunk.js"
+  },
+  {
+    "revision": "f301186a82072590bec4",
+    "url": "/react-movies-gallery/static/js/runtime-main.776f4273.js"
+  }
+]);
